61fac3a03b806517cfc84c7bff472fbf337a37af
Jalilov <javohirjalilov777@gmail.com>
2021-06-15 15:33:56 +0500

    add pages product and customer

--- App1/views.py
+++ App1/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse("<h3 align='center'>HOME PAGE APP_1</h3>")
+	return HttpResponse("<h2 align='center'>HOME PAGE APP_1</h2>")
 
 def about(request):
-    return HttpResponse("<h3 align='center'>ABOUT PAGE APP_1</h3>")
+    return HttpResponse("<h2 align='center'>ABOUT PAGE APP_1</h2>")
+
+def product(request):
+    return HttpResponse("<h2 align='center'>PRODUCT PAGE APP_1</h2>")
+
+def customer(request):
+    return HttpResponse("<h2 align='center'>CUSTOMER PAGE APP_1</h2>")
