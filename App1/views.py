e301958225329dc39fc76ba01e9db84a908af749
Jalilov <javohirjalilov777@gmail.com>
2021-06-15 15:25:44 +0500

    create About page

--- App1/views.py
+++ App1/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse("HOME PAGE APP_1")
+	return HttpResponse("<h3 align='center'>HOME PAGE APP_1</h3>")
+
+def about(request):
+    return HttpResponse("<h3 align='center'>ABOUT PAGE APP_1</h3>")
